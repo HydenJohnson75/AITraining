a032947aac0926873dedd4c825a818a951b44324
Hyden Johnson <78604065+HydenJohnson75@users.noreply.github.com>
2024-02-29 00:45:54 +0000

    Changed input for mouse look and reverted to left right camera looking for agent

--- Assets/ML-Agents/Move_Look_Agnt.cs
+++ Assets/ML-Agents/Move_Look_Agnt.cs
@@ -131,8 +131,7 @@ public class Move_Look_Agnt : Agent
 
         float moveDirection = actions.ContinuousActions[0];
         float moveRightDirection = actions.ContinuousActions[1];
-        float rotateDirection = actions.ContinuousActions[2];
-        float rotateUpDirection = actions.ContinuousActions[3];
+
 
         // Calculate movement based on input
         Vector3 move = transform.forward * moveDirection * moveSpeed * Time.deltaTime;
@@ -141,36 +140,7 @@ public class Move_Look_Agnt : Agent
         // Apply movement
         rb.MovePosition(transform.position + move);
 
-        // Calculate rotation based on input
-        float rotation = rotateDirection * rotationSpeed * Time.deltaTime;
-        float rotationUp = rotateUpDirection * rotationSpeed * Time.deltaTime;
 
-        // Apply rotation to the agent's Y-axis (horizontal rotation)
-        transform.Rotate(0, rotation, 0);
-
-        // Rotate the camera based on vertical input
-        xRotation -= rotationUp;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Clamp vertical rotation to prevent flipping
-        mainCam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-        rayObject.transform.localRotation = mainCam.transform.localRotation;
-
-        if(isLookingAtFloor)
-        {
-            AddReward(-1f/MaxStep);
-        }
-        else
-        {
-            AddReward(2f/MaxStep);
-        }
-
-        if(isLookingAtTarget)
-        {
-            AddReward(10f/MaxStep);
-        }
-        else
-        {
-            AddReward(-0.3f/MaxStep);
-        }
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -180,8 +150,7 @@ public class Move_Look_Agnt : Agent
         // Implement manual control for testing.
         continousAction[0] = Input.GetAxis("Vertical");
         continousAction[1] = Input.GetAxis("Horizontal");
-        continousAction[2] = Input.GetAxis("Horizontal2"); // Left/Right
-        continousAction[3] = Input.GetAxis("Vertical2");
+
 
     }
 
@@ -198,7 +167,6 @@ public class Move_Look_Agnt : Agent
         }
         else if (collision.gameObject.tag == "Enemy" && isLookingAtTarget == false)
         {
-            AddReward(0.5f);
             floorMeshRenderer.material = looseMaterial;
             touchedTarget = false;
             EndEpisode();
@@ -206,7 +174,7 @@ public class Move_Look_Agnt : Agent
 
         if (collision.gameObject.tag == "Wall")
         {
-            AddReward(-1.5f);
+            AddReward(-0.5f);
             floorMeshRenderer.material = looseMaterial;
             touchedTarget = false;
             EndEpisode();
